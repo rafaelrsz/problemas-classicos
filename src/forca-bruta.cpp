c8aa272442b2cef59d6cc1c114e4b7ce532cf4fa
rafaelrsz <rafa.rsouz@gmail.com>
2024-08-09 18:17:01 -0300

    refatoração final alg força bruta

--- src/forca-bruta.cpp
+++ src/forca-bruta.cpp
@@ -2,21 +2,23 @@
 #include <vector>
 #include <algorithm>
 #include <memory>
+#include <chrono>
 
 using namespace std;
 
 #define INF 10000000
 
 typedef vector<int> vi;
-typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
 
-int calculaCorte(int **MA, int n, vi particaoA, vi particaoB)
+int calculaCorte(vvi &MA, int n, vi &particaoA, vi &particaoB)
 {
   int valorCorte = 0;
 
   for (int i = 0; i < n / 2; i++)
   {
     int verticeA = particaoA[i];
+
     for (int j = 0; j < n / 2; j++)
     {
       int verticeB = particaoB[j];
@@ -29,33 +31,26 @@ int calculaCorte(int **MA, int n, vi particaoA, vi particaoB)
   return valorCorte;
 }
 
-pair<vi, vi> particionaForcaBruta(int **MA, int n)
+pair<vi, vi> particionaForcaBruta(vvi &MA, int n, int &corteMinimo)
 {
   pair<vi, vi> particoesOtimas;
-  vb estaNaParticaoA;
+  vi estaNaParticaoA(n);
+  fill_n(estaNaParticaoA.begin(), n / 2, 1);
 
-  // Inicializa partições iniciais
-  estaNaParticaoA.assign(n, false);
-  fill_n(estaNaParticaoA.begin(), n / 2, true);
-
-  int corteMinimo = INF;
+  corteMinimo = INF;
   // Gera todas as combinações possiveis para encontrar a melhor
   do
   {
-    vi particaoA(n / 2), particaoB(n / 2);
-    int indexA = 0, indexB = 0;
+    vi particaoA, particaoB;
+    particaoA.reserve(n / 2);
+    particaoB.reserve(n / 2);
+
     for (int i = 0; i < n; i++)
     {
       if (estaNaParticaoA[i])
-      {
-        particaoA[indexA] = i;
-        indexA++;
-      }
+        particaoA.push_back(i);
       else
-      {
-        particaoB[indexB] = i;
-        indexB++;
-      }
+        particaoB.push_back(i);
     }
 
     int corte = calculaCorte(MA, n, particaoA, particaoB);
@@ -65,31 +60,39 @@ pair<vi, vi> particionaForcaBruta(int **MA, int n)
       corteMinimo = corte;
       particoesOtimas = make_pair(particaoA, particaoB);
     }
-    // Verifica se estaNaParticaoA[0] para evitar partições equivalentes
-  } while (prev_permutation(estaNaParticaoA.begin(), estaNaParticaoA.end()) && estaNaParticaoA[0]);
+
+  } while (
+      std::prev_permutation(estaNaParticaoA.begin(), estaNaParticaoA.end()) &&
+      estaNaParticaoA[0]); // A validação estaNaParticaoA[0] evita partições equivalentes
 
   return particoesOtimas;
 }
 
+void exibirParticoes(pair<vi, vi> &particoesOtimas, int n)
+{
+  cout << "Particao A: ";
+  for (int i = 0; i < n / 2; i++)
+  {
+    cout << particoesOtimas.first[i] << " ";
+  }
+
+  cout << endl
+       << "Particao B: ";
+  for (int i = 0; i < n / 2; i++)
+  {
+    cout << particoesOtimas.second[i] << " ";
+  }
+  cout << endl;
+}
+
 int main()
 {
+  // Inicialização das estruturas de dados e leitura dos dados
   int n, m, u, v;
   cin >> n >> m;
 
-  int **MA = new int *[n];
+  vvi MA(n, vi(n, 0));
 
-  // Inicialização da matriz de adjacência
-  for (int i = 0; i < n; i++)
-  {
-    MA[i] = new int[n];
-
-    for (int j = 0; j < n; j++)
-    {
-      MA[i][j] = 0;
-    }
-  }
-
-  // Leitura dos dados
   for (int i = 0; i < m; i++)
   {
     cin >> u >> v;
@@ -97,35 +100,18 @@ int main()
   }
 
   // Execução do algoritmo
-  pair<vi, vi> particoesOtimas = particionaForcaBruta(MA, n);
+  int corteMinimo;
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+  pair<vi, vi> particoesOtimas = particionaForcaBruta(MA, n, corteMinimo);
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-  // Exibição do resultado *CALCULO DUPLICADO DO MELHOR CORTE - MELHORAR*
-  cout << "O valor do corte minimo eh "
-       << calculaCorte(MA, n, particoesOtimas.first, particoesOtimas.second)
+  cout << "Valor do corte minimo:"
+       << corteMinimo
        << endl;
 
-  cout << "Particao A: ";
-  for (int i = 0; i < n / 2; i++)
-  {
-    cout << particoesOtimas.first[i] << "  ";
-  }
+  exibirParticoes(particoesOtimas, n);
 
-  cout << endl
-       << "Particao B: ";
-  for (int i = 0; i < n / 2; i++)
-  {
-    cout << particoesOtimas.second[i] << "  ";
-  }
-  cout << endl;
+  cout << "Tempo de execução = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " ms" << std::endl;
 
-  // Desalocação de memória
-  for (int i = 0; i < n; i++)
-  {
-    delete MA[i];
-  }
-
-  delete MA;
-
-  system("pause");
   return 0;
 }
